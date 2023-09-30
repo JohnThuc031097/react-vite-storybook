f8e5a71ce92e7f51b339ddca58a3b74ee167d235
ThucNobita <johnthuc1997@gmail.com>
2023-09-30 13:42:01 +0700

    init

--- src/components/Navbar/Navbar.stories.jsx
+++ src/components/Navbar/Navbar.stories.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./Navbar";
 
+
 export default {
     component: Navbar,
     title: 'Navbar',
-    tags: ['autodocs'],
+    // tags: ['autodocs'],
 };
 
 export const Default = {
