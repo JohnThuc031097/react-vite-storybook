6a04a2d214e70c29a21fae18638267decf99e7db
ThucNobita <johnthuc1997@gmail.com>
2023-10-09 15:48:01 +0700

    init

--- src/components/Navbar/TNavbar.jsx
+++ src/components/Navbar/TNavbar.jsx
@@ -1,20 +1,121 @@
 
-import { Box } from "@mui/material";
-import { IconSearch } from "../Icons/IconSearch";
+import { Logout, NotificationsNoneOutlined, Person, Settings } from "@mui/icons-material";
+import {
+    Box, Stack,
+    IconButton,
+    Avatar,
+    Menu, MenuItem,
+    Divider,
+    ListItemIcon
+} from "@mui/material";
+
+import { useState } from 'react'
 
 export default function TNavbar() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <Box
-            sx={{
-                flexGrow: 1,
-                backgroundColor: 'white',
-                width: '100%',
-                height: '150px',
-                padding: '10px 10px 1px 10px',
-                borderRadius: '20px'
-            }}
-        >
-
-        </Box>
+        <>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    backgroundColor: 'white',
+                    width: '800px',
+                    height: '40px',
+                    padding: '10px 10px 10px 10px',
+                    borderRadius: '20px'
+                }}
+            >
+                <Stack
+                    direction="row"
+                    justifyContent="right"
+                    alignItems="center"
+                    spacing={1}
+                >
+                    <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{ ml: 2 }}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <NotificationsNoneOutlined sx={{ width: 32, height: 32 }} />
+                    </IconButton>
+                    <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{ ml: 2 }}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <Avatar sx={{ width: 34, height: 34 }}>T</Avatar>
+                    </IconButton>
+                </Stack>
+            </Box>
+            <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        width: '200px',
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Person fontSize="small" />
+                    </ListItemIcon>
+                    Profile
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </>
     );
 }
